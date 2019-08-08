799df460823ccba5f984aa77adc7323230850784
Igor Cerqueira <igor_vc@hotmail.com>
2019-08-08 20:50:26 -0300

    configurando login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
 
 import Logo from '../assets/logo.svg'
 
 export default function Login() {
+    const [username, setUsername] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        console.log(username)
+    }
+
+
     return(
     <div className="login-container">
-        <form>
+        <form onSubmit={handleSubmit}>
             <img src={Logo} alt='Tindev' />
-            <input type="text" placeholder="digite um texto"></input>
+            <input 
+            placeholder="digite um texto"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            />
             <button type="submit">Enviar</button>
         </form>
     </div>
