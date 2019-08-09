baf513f6ca2b9640bf9d7b984e5dc183142d8af5
Igor Cerqueira <igor_vc@hotmail.com>
2019-08-08 21:36:54 -0300

    finalizando etapa usuarios

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Main.css'
+import api from '../services/api'
 
 import Logo from '../assets/logo.svg'
 import dislike from '../assets/dislike.svg'
@@ -7,26 +8,41 @@ import like from '../assets/like.svg'
 
 //recuperar id do usuario e mostrar em tela
 export default function Main({ match }){
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs', {
+                headers: { user: match.params.id }
+            })
+            setUsers(response.data)
+        }
+
+        loadUsers()
+    }, [match.params.id])
+
     return(
         <div className="main-container">
             <img src={Logo} alt='Tindev' />
             <ul>
-                <li>
-                    <img src="https://avatars3.githubusercontent.com/u/40477878?v=4" />
-                    <footer>
-                        <strong>Igor Vieira</strong>
-                        <p>Programador e Dj nos tempos livres.</p>
-                    </footer>
+                {users.map(user =>(
+                    <li key={user._id}>
+                        <img src={user.avatar} alt={user.name}/>
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="deslike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>    
+                        <div className="buttons">
+                            <button type="button">
+                                <img src={dislike} alt="deslike" />
+                            </button>
+                            <button type="button">
+                                <img src={like} alt="like" />
+                            </button>
+                        </div>
+                    </li>    
+                ))}
             </ul>
         </div>
     )
