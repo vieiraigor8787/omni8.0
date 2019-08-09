ca5906906e630510614120e5075da8d4bda21681
Igor Cerqueira <igor_vc@hotmail.com>
2019-08-08 21:20:44 -0300

    configurando conteudo main

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import './Main.css'
 
-export default function Main(){
-    return <h1>Hello world</h1>
+import Logo from '../assets/logo.svg'
+import dislike from '../assets/dislike.svg'
+import like from '../assets/like.svg'
+
+//recuperar id do usuario e mostrar em tela
+export default function Main({ match }){
+    return(
+        <div className="main-container">
+            <img src={Logo} alt='Tindev' />
+            <ul>
+                <li>
+                    <img src="https://avatars3.githubusercontent.com/u/40477878?v=4" />
+                    <footer>
+                        <strong>Igor Vieira</strong>
+                        <p>Programador e Dj nos tempos livres.</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="deslike" />
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="like" />
+                        </button>
+                    </div>
+                </li>    
+            </ul>
+        </div>
+    )
 }
